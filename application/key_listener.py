8f3f512f271af70aa9811d1cbbec82d90ce27765
Litefallen <nolife0808@gmail.com>
2024-05-26 18:04:12 +0200

    Pressing Esc button cancels key listening and redirects to the main page

--- application/key_listener.py
+++ application/key_listener.py
@@ -1,12 +1,11 @@
 from pynput import keyboard
-
+from flask import render_template,redirect, url_for,request
 
 def randdom_func(expected_key):
     if expected_key == '_':
         expected_key = 'space'
     the_key = None
-    print(expected_key)
-    def on_press(key):
+    def on_press(key):              
         try:
             nonlocal the_key
             the_key = key.char
@@ -14,17 +13,21 @@ def randdom_func(expected_key):
             return False
         except AttributeError:
             the_key = key.name
-            print(the_key == expected_key)
-            if key.name == 'esc':
-                exit()
-            print(key.name)
+            # print(the_key == expected_key)
+            # print('special key was pressed')     
             return False
+
 
     with keyboard.Listener(
         on_press=on_press,
         # on_release=on_release,
-        suppress=True) as listener:
+        suppress=False) as listener:
         listener.join()
     print(f"The pressed key is: {the_key}, the expected key is: {expected_key}")
-    print(the_key== expected_key)
-    return {'result' :True} if the_key== expected_key else {'result' :False}
+    # print(the_key== expected_key)
+    if the_key== expected_key:
+        return {'result' :True}
+    if the_key == 'esc':
+        return {'result' :'Abort'}
+    else:
+        return {'result' :False}
