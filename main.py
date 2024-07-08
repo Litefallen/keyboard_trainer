c9a0279a2ca85639bcaf1ec1f905df84b61d7127
Litefallen <nolife0808@gmail.com>
2024-07-08 20:02:13 +0200

    Everything works, cleaned code, removed unused files, updated requirements.txt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from application import create_app
 import os
-from flask_cors import CORS
 app = create_app()
-CORS(app)
 if __name__ == '__main__':
-
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8080)
     
